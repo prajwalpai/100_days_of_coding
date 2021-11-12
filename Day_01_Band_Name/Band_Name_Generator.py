0b62376bf6514125b8d923053de5d9f5675fafa9
prajpai <prajpai@cisco.com>
2021-11-13 02:33:27 +0530

    Day-1-Complete

---
+++ Day_01_Band_Name/Band_Name_Generator.py
@@ -0,0 +1,5 @@
+
+print("Welcome to Band Name Generator.")
+city=input('Which city were you born in : ')
+pet=input("What was your first pet's name: ")
+print("Your fun Band name can be "+city+" "+pet)
---
+++ Day_01_Band_Name/sandbox.py
@@ -0,0 +1,4 @@
+#print("Hello World")
+
+print("Day 1 - Python Print Function\n\nThe function is declared like this:\n\nprint('what to print')")
+
