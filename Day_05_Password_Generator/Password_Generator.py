1dcdd477345fc2ce7763c3c4481fc42bf22ec0ca
prajpai <prajpai@cisco.com>
2022-01-04 12:38:37 +0530

    Day5 Commits done

---
+++ Day_05_Password_Generator/Password_Generator.py
@@ -0,0 +1,26 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password=''
+for letter in range((nr_letters+nr_symbols+nr_numbers)):
+    if nr_letters:
+        password += random.choice(letters)
+        nr_letters = nr_letters - 1
+    if nr_symbols:
+        password += random.choice(symbols)
+        nr_symbols = nr_symbols - 1
+    if nr_numbers:
+        password += random.choice(numbers)
+        nr_numbers = nr_numbers - 1
+password_list = list(password)
+random.shuffle(password_list)
+result = ''.join(password_list)
+print("Your password can be : "+result)
